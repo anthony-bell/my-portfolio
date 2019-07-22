d5932ece23b9f6c9231be4a2b97835cb17e6e557
AJ <anthonyjbell21@gmail.com>
2019-07-22 12:38:52 -0700

    update about me page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,13 +1,135 @@
 import React, {Component} from "react";
+import { SchoolIcon } from '@material-ui/icons/School';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import {Tabs , Tab} from "react-mdl";
 
 class AboutMe extends Component {
+    constructor(props){
+        super(props);
+        this.state = { activeTab: 0 };
+    }
+
+    toggleCategories() {
+        if(this.state.activeTab === 0){
+            return(
+                <div><h1>Summary</h1></div>
+            )
+        }else if (this.state.activeTab ===1){
+            return(
+                <div>
+                <VerticalTimeline>
+
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="2011 - present"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        // icon={<WorkIcon />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Creative Director</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+                        <p>
+                            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="2010 - 2011"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        // icon={<WorkIcon />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Art Director</h3>
+                        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                        <p>
+                            Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="2008 - 2010"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        // icon={<WorkIcon />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+                        <p>
+                            User Experience, Visual Design
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="2006 - 2008"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        // icon={<WorkIcon />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                        <p>
+                            User Experience, Visual Design
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        date="April 2013"
+                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                        // icon={<SchoolIcon />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+                        <p>
+                            Strategy, Social Media
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        date="November 2012"
+                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                        // icon={<SchoolIcon />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Certification</h4>
+                        <p>
+                            Creative Direction, User Experience, Visual Design
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        date="2002 - 2006"
+                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                        // icon={<SchoolIcon />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+                        <p>
+                            Creative Direction, Visual Design
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+                        // icon={<StarIcon />}
+                    />
+
+                </VerticalTimeline>
+                </div>
+            )
+        }
+    }
     render() {
         return(
-            <div>
-                <h1>AboutMe Page</h1>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId})} ripple>
+                    <Tab>Summary</Tab>
+                    <Tab>Experience</Tab>
+                </Tabs>
+
+                <section classname="aboutme-grid">
+                    {this.toggleCategories()}
+                </section>
             </div>
+
         )
     }
 }
 
-export default AboutMe
+export default AboutMe;
