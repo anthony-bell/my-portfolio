ed6bd459c6fdf9cdc0d8181ecaba6f34359ccc00
AJ <anthonyjbell21@gmail.com>
2019-09-03 13:38:56 -0700

    Updates

---
+++ precache-manifest.c6796ab580b4d66e1b4831bf325885bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "99be3541c67488932803",
+    "url": "/my-portfolio/static/css/main.8349e38a.chunk.css"
+  },
+  {
+    "revision": "99be3541c67488932803",
+    "url": "/my-portfolio/static/js/main.ca8bcfa6.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  },
+  {
+    "revision": "472ca00fb1272fc46f8f",
+    "url": "/my-portfolio/static/css/2.86a72812.chunk.css"
+  },
+  {
+    "revision": "472ca00fb1272fc46f8f",
+    "url": "/my-portfolio/static/js/2.ba373f7a.chunk.js"
+  },
+  {
+    "revision": "e77a85434424995daf3d5164dc7c788a",
+    "url": "/my-portfolio/static/media/resumeAnthony.e77a8543.pdf"
+  },
+  {
+    "revision": "5ebb7ebafe23179cc7dec7b9bcd95df5",
+    "url": "/my-portfolio/index.html"
+  }
+];
